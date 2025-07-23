ecdd84412de37da82acd306dee10a8968d5c44fd
Nate <37554478+servusdei2018@users.noreply.github.com>
2025-07-22 23:35:15 -0400

    nit: create init.sh

---
+++ init.sh
@@ -0,0 +1,4 @@
+# Initial configurations to apply to a fresh system
+
+# Ignore lifecycle scripts by default (https://www.nodejs-security.com/blog/npm-ignore-scripts-best-practices-as-security-mitigation-for-malicious-packages)
+npm config set ignore-scripts true
