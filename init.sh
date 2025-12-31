716ef74e3017de334e79fd4116213897ea4c0020
Nate <37554478+servusdei2018@users.noreply.github.com>
2025-12-31 16:28:46 -0500

    init.sh: add git config

--- init.sh
+++ init.sh
@@ -2,3 +2,7 @@
 
 # Ignore lifecycle scripts by default (https://www.nodejs-security.com/blog/npm-ignore-scripts-best-practices-as-security-mitigation-for-malicious-packages)
 npm config set ignore-scripts true
+
+# Git config
+git config --global user.name "Nate Bracy"
+git config --global user.email "nate@bracy.dev"
